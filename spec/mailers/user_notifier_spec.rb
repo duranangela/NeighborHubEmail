95623eb7760e12c807ff24ab6504336967b50a6b
Angela Duran <duranangela19@gmail.com>
2018-10-31 15:00:36 -0600

    Add mailer tests

--- spec/mailers/user_notifier_spec.rb
+++ spec/mailers/user_notifier_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe UserNotifierMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'verify' do
+    let(:email)  {'jorj@jorj.com'}
+    let(:name) {'jorj'}
+    let(:mail) {described_class.inform(name, email).deliver_now}
+
+    it 'renders subject' do
+      expect(mail.subject).to eq('Welcome, jorj!')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['no-reply@neighborhub.com'])
+    end
+
+  end
+
 end
